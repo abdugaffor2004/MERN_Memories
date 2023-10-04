cc5e61701c150c56bb2dbe4f97e41ea61f53a7b6
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-10-04 16:37:08 +0300

    feat: styled the posts

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,10 +1,41 @@
 import useStyles from "./PostStyle"
+import { Card, CardActions, CardMedia, CardContent, Button, Typography } from "@mui/material";
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'; // marerial-ui icons
 
-const Post = () =>{
+import moment from 'moment'; // from libriary to fetch the current time
+
+const Post = (props) =>{
     const {classes} = useStyles();
     
     return(
-        <h2>Post</h2>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={props.selectedFile} title={props.title} />
+
+            <div className={classes.overlay}>
+                <Typography variant="h6">{props.creator}</Typography>
+                <Typography variant="body2"> { moment(props.createdAt).fromNow() } </Typography>
+            </div>
+
+            <div className={classes.overlay2}>
+                <Button style={{color:"white"}} size="small" onClick={() =>{}}> <MoreHorizIcon  /> </Button>
+            </div>
+
+            <div className={classes.details}>
+                <Typography variant="body2" color='textSecondary'>{props.tags.map( (item) => `# ${item} `)}</Typography>
+            </div>
+
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>{props.message}</Typography>
+            </CardContent>
+
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={()=>{}}> <ThumbUpAltIcon fontSize="small" /> Like {props.likeCount} </Button>
+                <Button size="small" color="primary" onClick={()=>{}}> <DeleteIcon /> Delete</Button>
+            </CardActions>
+
+        </Card>
     )
 }
 
--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -2,16 +2,42 @@ import { useSelector } from "react-redux";
 import Post from "./Post/Post"
 
 import useStyles from "./PostsStyle"
+import { CircularProgress, Grid } from "@mui/material";
 
 
 const Posts = () =>{
     const {classes} = useStyles();
-    const postsReducer = useSelector((state) => state.post) // is brother of connect() for extracting data from state
+    const {posts} = useSelector((state) => state.post) // is brother of connect() for extracting data from state
+    console.log(posts)
     return(
         <>
             <h1> Posts </h1>
-            <Post />
-            <Post />
+
+            {
+
+            posts.length === 0 ? <CircularProgress style={{position: "absolute", top: '50%', left: "30%"}}/> : (
+                <Grid marginTop={3} className={classes.mainContainer}  alignItems="stretch" gap={3}>
+                    {
+                        posts.map( (item) => 
+                        <Grid key={item._id} xs={12} sm={6} > 
+                            <Post id={item._id}
+                                  creator={item.creator}
+                                  title={item.title}
+                                  likeCount={item.likeCount}
+                                  message={item.message}
+                                  selectedFile={item.selectedFile}
+                                  tags={item.tags}
+                                  createdAt={item.createdAt}
+                                  /> 
+                                  
+                        </Grid>  )
+                    }
+                </Grid>
+            )
+            
+
+            }
+            
         </>
         
     )
