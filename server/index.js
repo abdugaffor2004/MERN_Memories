20cce343def80980515daf5b8722bd54379abe7d
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-10-15 21:43:20 +0300

    fixed env

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 import postRouter from "./routes/posts.js";
-
+import dotenv from 'dotenv';
 
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: '30mb', extended: true})) // Limit for sending imgs
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
@@ -16,10 +17,9 @@ app.use(cors()) // for cross origin requests to anotherbrowser url
 
 app.use("/posts", postRouter) // added posts prefix to all routes in postRouter
 
-const CONNECTION_URL = "mongodb+srv://guraba_01:guraba123@clustermemories.z0mwcgf.mongodb.net/?retryWrites=true&w=majority"; // from mongoDB atlas
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true})
 .then( () => app.listen(PORT, () => console.log( `Server is running on ${PORT}` )) ) // if connection to database is successful
 .catch( (error) => console.log(error.message) )
 
